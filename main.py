dad3fef03233b8eca89ec529073289d1dcc31983
manaswinidas <dasmanaswini10@gmail.com>
2017-09-09 15:58:06 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,97 @@
+import pandas
+from sklearn.cross_validation import train_test_split
+import numpy as np
+import time
+from sklearn.externals import joblib
+import Recommenders as Recommenders
+import Evaluation as Evaluation
+
+#Read userid-songid-listen_count triplets
+#This step might take time to download data from external sources
+triplets_file = 'data.txt'
+songs_metadata_file = 'song_data.csv'
+
+song_df_1 = pandas.read_table(triplets_file,header=None)
+song_df_1.columns = ['user_id', 'song_id', 'listen_count']
+
+#Read song  metadata
+song_df_2 =  pandas.read_csv(songs_metadata_file)
+
+#Merge the two dataframes above to create input dataframe for recommender systems
+song_df = pandas.merge(song_df_1, song_df_2.drop_duplicates(['song_id']), on="song_id", how="left")
+
+print(song_df.head())
+
+len(song_df)
+
+song_df = song_df.head(10000)
+
+#Merge song title and artist_name columns to make a merged column
+song_df['song'] = song_df['title'].map(str) + " - " + song_df['artist_name']
+
+song_grouped = song_df.groupby(['song']).agg({'listen_count': 'count'}).reset_index()
+grouped_sum = song_grouped['listen_count'].sum()
+song_grouped['percentage']  = song_grouped['listen_count'].div(grouped_sum)*100
+song_grouped.sort_values(['listen_count', 'song'], ascending = [0,1])
+
+users = song_df['user_id'].unique()
+
+len(users)
+
+###Fill in the code here
+songs = song_df['song'].unique()
+len(songs)
+
+train_data, test_data = train_test_split(song_df, test_size = 0.20, random_state=0)
+print(train_data.head(5))
+
+pm = Recommenders.popularity_recommender_py()
+pm.create(train_data, 'user_id', 'song')
+
+user_id = users[10]
+pm.recommend(user_id)
+
+is_model = Recommenders.item_similarity_recommender_py()
+is_model.create(train_data, 'user_id', 'song')
+
+#Print the songs for the user in training data
+user_id = users[5]
+user_items = is_model.get_user_items(user_id)
+#
+print("------------------------------------------------------------------------------------")
+print("Training data songs for the user userid: %s:" % user_id)
+print("------------------------------------------------------------------------------------")
+
+for user_item in user_items:
+    print(user_item)
+
+print("----------------------------------------------------------------------")
+print("Recommendation process going on:")
+print("----------------------------------------------------------------------")
+
+#Recommend songs for the user using personalized model
+is_model.recommend(user_id)
+
+user_id = users[7]
+#Fill in the code here
+user_items = is_model.get_user_items(user_id)
+#
+print("------------------------------------------------------------------------------------")
+print("Training data songs for the user userid: %s:" % user_id)
+print("------------------------------------------------------------------------------------")
+
+for user_item in user_items:
+    print(user_item)
+
+print("----------------------------------------------------------------------")
+print("Recommendation process going on:")
+print("----------------------------------------------------------------------")
+
+#Recommend songs for the user using personalized model
+is_model.recommend(user_id)
+
+is_model.get_similar_items(['U Smile - Justin Bieber'])
+
+song = 'Yellow - Coldplay'
+###Fill in the code here
+is_model.get_similar_items([song])
